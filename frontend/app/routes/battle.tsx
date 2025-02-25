b5a93ae08bbfdaee744c0e7a8d17a8c580453eba
shoma883 <shouma.s.0509@gmail.com>
2025-02-25 18:16:38 +0900

    ルーティング設計

---
+++ frontend/app/routes/battle.tsx
@@ -0,0 +1,8 @@
+export default function Battle() {
+  return (
+    <div>
+      <h1>バトル画面</h1>
+      <p>ここでバトルを行います（仮）</p>
+    </div>
+  );
+}
---
+++ frontend/app/routes/fishing.tsx
@@ -0,0 +1,8 @@
+export default function Fighing() {
+    return (
+        <div>
+            <h1>釣り画面</h1>
+            <p>ここで釣りを行います</p>
+        </div>
+    )
+}
